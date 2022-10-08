9a053577700ea92ff91e670202e749a1b65451b4
zocker-160 <zocker1600@posteo.net>
2022-10-08 16:59:54 +0200

    uncomment build command in local build script

--- localBuild.sh
+++ localBuild.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env python3
 
-#flatpak-builder --force-clean --user --install build/ org.tlauncher.TLauncher.yml
+flatpak-builder --force-clean --user --install build/ org.tlauncher.TLauncher.yml
 flatpak build-bundle ~/.local/share/flatpak/repo TLauncher.flatpak org.tlauncher.TLauncher
