e5462bea37557f4a8e232fe698636e04dd2e149e
zocker-160 <zocker1600@posteo.net>
2023-08-10 13:07:43 +0200

    update to TL 2.885

--- localBuild.sh
+++ localBuild.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
 
-flatpak-builder --force-clean --user --install build/ org.tlauncher.TLauncher.yml
+#flatpak-builder
+flatpak run org.flatpak.Builder --force-clean --user --install build/ org.tlauncher.TLauncher.yml
 flatpak build-bundle ~/.local/share/flatpak/repo TLauncher.flatpak org.tlauncher.TLauncher
